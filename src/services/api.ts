f293a528e75100502ea61d8e6b372556323b4d1b
tonnyrogers <antoniel15975@gmail.com>
2022-03-28 12:58:50 -0300

    api url + app version

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import * as RNLocalize from 'react-native-localize';
 
-const base = __DEV__ ? 'http://127.0.0.1:3333' : 'https://api.rotery.com.br';
+const base = __DEV__
+  ? 'http://127.0.0.1:3333'
+  : 'https://api.staging.rotery.com.br';
 const timezoneName = RNLocalize.getTimeZone();
 
 const api = axios.create({
